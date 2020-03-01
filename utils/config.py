df2d52237554a9cb2d79b6c6ccd8e4b38b6a3a5a
Niranjan <niranjan.deshpande187@gmail.com>
2020-03-01 20:21:23 +0100

    - Class created to represent configurations.

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+class Config(object):
+
+    def __init__(self):
+        self.seed = None
+        self.environment = None
+        self.state_dim = None
+        self.action_dim = None
+        self.num_of_episodes = None
+        self.num_of_steps_per_episode = None
+        self.use_cuda = None 
+
+        self.hyperparameters = None
