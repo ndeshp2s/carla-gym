a223bd06ceab4d2f14b0cc0a5945bcda139c1825
niranjan <niranjan.deshpande187@gmail.com>
2020-03-02 01:36:43 +0100

    - Class for representing configurations.

---
+++ experiments/config.py
@@ -0,0 +1,12 @@
+class Config:
+    env: str = None
+    hyperparameters = None
+    state_dim: int = None
+    action_dim: int = None
+    use_cuda = None
+    number_of_episodes = 0
+    steps_per_episode = 0
+    current_episode = 0
+    output = 'logs'
+
+
