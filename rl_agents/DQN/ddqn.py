42e554fdce03f5c37c69f857d553d046b5efaa54
niranjan <niranjan.deshpande187@gmail.com>
2020-03-02 01:34:58 +0100

    - Renamed agents directory to rl_agents

---
+++ rl_agents/DQN/ddqn.py
@@ -0,0 +1,27 @@
+import torch
+from experiments.config import Config
+
+class DDQNAgent:
+    def __init__(self, config: Config):
+
+        # Parameter initialization
+        self.state_size = config.state_dim
+        self.action_ize = config.action_dim
+
+        # self.hyperparameters = config.hyperparameters
+
+        # self.step_number = 0
+
+        # if config.use_cuda:
+        #     self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # else:
+        #     self.device = "cpu"
+
+        # # Initialise Q-Network
+        # self.local_network = NeuralNetwork(self.state_size, self.action_size).to(device)
+        # self.target_network = NeuralNetwork(self.state_size, self.action_size).to(device)
+        # self.optimizer = optim.Adam(self.local_network.parameters(), lr = self.hyperparameters["learning_rate"])
+
+        # # Initialise replay memory
+        # self.memory = ReplayBuffer(self.hyperparameters["buffer_size"], self.hyperparameters["batch_size"], self.seed)
