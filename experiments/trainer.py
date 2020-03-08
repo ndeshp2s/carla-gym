f285c9926c8269be15b7accdefb05f093ad0cb15
niranjan <niranjan.deshpande187@gmail.com>
2020-03-08 23:57:14 +0100

    - New functionality added to create folder for saving logs.

--- experiments/trainer.py
+++ experiments/trainer.py
@@ -1,3 +1,5 @@
+import os
+import torch
 from torch.utils.tensorboard import SummaryWriter
 
 from experiments.config import Config
@@ -10,8 +12,11 @@ class Trainer:
         self.agent = agent
         self.config = config
 
-        self.epsilon_decay = EpsilonTracker(self.config.hyperparameters["epsilon_start"], self.config.hyperparameters["epsilon_end"], self.config.hyperparameters["min_steps_before_learning"], self.config.number_of_episodes * self.config.steps_per_episode)
+        self.epsilon_decay = EpsilonTracker(self.config.hyperparameters["epsilon_start"], self.config.hyperparameters["epsilon_end"], 
+            self.config.hyperparameters["min_steps_before_learning"], self.config.number_of_episodes * self.config.steps_per_episode)
 
+        if not os.path.isdir(self.config.log_dir):
+            os.makedirs(self.config.log_dir)
         self.writer = SummaryWriter(log_dir = self.config.log_dir)
 
     def train(self, previous_episode = 0):
@@ -23,43 +28,62 @@ class Trainer:
         
         epsilon = self.config.hyperparameters["epsilon_start"]
 
-        for e in range(previous_episode + 1, self.config.number_of_episodes + 1):
+        for ep_num in range(previous_episode + 1, self.config.number_of_episodes + 1):
+
+            # Reset the environment and variables for new episode
+            episode_reward = 0
             state = self.env.reset()
 
-            for s in range(self.config.steps_per_episode):
-            
+            for step_num in range(50):
+                
+                # Select action
                 action = '2'
+
+                # Execute step
                 next_state, reward, done = self.env.step(action)
 
-                self.agent.add(state, action, reward, next_state)
+                # Add experience to memory of local network
+                self.agent.add(state, action, reward, next_state, done)
 
-            #   loss = self.agent.step(state, action, reward, next_state, done)
+                # Update parameters
+                state = next_state
+                episode_reward += reward
 
-            #   if loss is not None:
-            #       losses.append(loss)
-            #       self.writer.add_scalar('Loss per step', loss, s) # value, step
+                # Performing learning if minumum required experiences gathered
+                loss = 0
 
-            #   if done:
-            #       episode_reward = 0
-            #       break
+                if done:
+                    break
 
-            #   state = next_state
-
-            #   episode_reward += reward
-
-            #   # epsilon update
-            #   epsilon = self.epsilon_decay(s)
+                # epsilon update
+                epsilon = self.epsilon_decay.update()
 
 
-            episode_number += 1
+            # Print details of the episode
+            print("------------------------------------------------")
+            print("Episode: %d, Reward: %5f, Loss: %4f" % (ep_num, episode_reward, loss))
+            print("------------------------------------------------")
 
-            print("Episode: %4d, Reward: %5f, Loss: %4f" % episode_number, episode_reward, loss)
-
-            self.writer.add_scalar('Reward per episode', episode_reward, episode_number)
+            # Save episode reward and loss
+            self.writer.add_scalar('Reward per episode', episode_reward, ep_num)
 
             # Save weights
-            if self.config.checkpoint and e % self.config.checkpoint_interval == 0:
-                self.agent.save_checkpoint()
+            self.agent.save_model(self.config.model_dir)
+
+            # Save checkpoints
+            if not os.path.isdir(self.config.checkpoint_dir):
+                os.makedirs(self.config.checkpoint_dir)
+
+            checkpoint = {'state_dict': self.agent.local_network.state_dict(),
+                        'optimizer': self.agent.optimizer.state_dict(),
+                        'episode': ep_num,
+                        'epsilon': epsilon}
+            torch.save(checkpoint, self.config.checkpoint_dir + '/model_and_parameters.pth')
+            
+            # if self.config.checkpoint and ep_num % self.config.checkpoint_interval == 0:
+            #     self.agent.save_checkpoint()
+
+
 
     def close(self):
         self.env.close()
