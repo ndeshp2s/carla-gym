ddf8975720c3c109b80255a1383981531b1bd9e1
Niranjan <niranjan.deshpande187@gmail.com>
2020-03-12 19:41:06 +0100

    - Sampling method modified.

--- rl_agents/DQN/replay_buffer.py
+++ rl_agents/DQN/replay_buffer.py
@@ -1,3 +1,5 @@
+import random
+import numpy as np
 import torch
 from collections import deque, namedtuple
 
@@ -12,14 +14,15 @@ class ReplayBuffer():
         experience = self.experience(state, action, reward, next_state, done)
         self.memory.append(experience)
 
-    def sample(self):
-        experiences = random.sample(self.memory, k = self.batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+    def sample(self, batch_size = 32):
+        experiences = random.sample(self.memory, k = batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
 
         return (states, actions, rewards, next_states, dones)
 
