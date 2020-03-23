96ff2f2bb9d2e2d430ab4482688c7d143230d62d
ndeshp2s <niranjan.deshpande187@gmail.com  git config --global user.name Your>
2020-03-23 01:49:19 +0100

    - New variables added.

--- environments/urban_environment/carla_config.py
+++ environments/urban_environment/carla_config.py
@@ -8,6 +8,10 @@ sp_roll = 0.0
 
 ev_bp = 'vehicle.audi.etron'
 ev_name = 'hero'
+ev_goal_x = -132.0
+ev_goal_y = 48.25
+ev_goal_z = 0.4
+ev_goal_yaw = -180.0
 
 # Sensors
 rgb_sensor = True
@@ -21,10 +25,10 @@ rgb_loc_z = 2.0
 sem_sensor = False
 
 # Display related
-display = True
+display = False
 
 # Rendering related
-render = True
+render = False
 screen_x = 720
 screen_y = 720
 
@@ -38,9 +42,17 @@ num_of_ped = 3
 HEIGHT = 1
 WIDTH = 4*num_of_ped
 N_CHANNELS = 0
-grid_height = 84
-grid_width = 84
-features = 1
+grid_height = 40
+grid_width = 30
+features = 3
+lane_types = 3# Driving(road)-1, sidewalk(shoulder)-2, crosswalk-3 
+
+x_min = -5
+x_max = 35
+x_size = 30
+y_min = -15
+y_max = 15
+y_size = 30
 
 # Pedestrian behavior probalities
 # Four types of behaviors:
@@ -52,11 +64,11 @@ features = 1
 ped_beh_prob = [1.0, 0.0, 0.0, 0.0,0.0] # 
 
 # Spawner related
-num_of_ped = 1
+num_of_ped = 10
 num_of_veh = 0
-percentage_pedestrians_crossing = 0.5
-percentage_pedestrians_crossing_illegal = 0.5
+percentage_pedestrians_crossing = 0.0
+percentage_pedestrians_crossing_illegal = 1.0
 ped_spawn_min_dist = -10.0
 ped_spawn_max_dist = 50.0
-ped_max_dist = 70.0
-ped_min_dist = -5.0
+ped_max_dist = 60.0
+ped_min_dist = -10.0
