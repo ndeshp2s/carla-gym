7bbb1251f68d2d7acbf378591c674395da084d51
Niranjan DESHPANDE <niranjan.deshpande187@gmail.com>
2020-04-20 04:45:12 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 sudo apt-get update
+
+sudo apt install python3-pip
+
+sudo pip3 install torch==1.3.0+cpu torchvision==0.4.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+sudo pip3 install tensorboard
+
+sudo pip3 install gym
+
+pip3 install matplotlib==2.0.0
+
+sudo apt-get install python3-tk 
+
+pip3 install transforms3d
+
+pip3 install psutil
+
+pip3 install shapely
+
+sudo pip3 install pygame
+
+pip3 install networkx
